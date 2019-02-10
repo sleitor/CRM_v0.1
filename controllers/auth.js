fba5aa27a4642663e38409b02b9dda6cdd157a56
DmitryT <8910140@mail.ru>
2019-02-11 00:09:26 +0300

    21.1 check encrypted password (login method)

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,26 @@
 const bcrypt = require('bcrypt');
 const {User} = require('../models/User');
 
-module.exports.login = (req, res) => {
+module.exports.login = async (req, res) => {
+  const {email, password} = req.body;
+
+  const candidate = User.findOne({email});
+
+  if (candidate) {
+    //do password check
+    const passwordCompare = bcrypt.compareSync(password, candidate.password);
+    if (passwordCompare) {
+      // create token
+    } else {
+      res.status(401).json({
+        message: 'Password didn\'t mach'
+      })
+    }
+  } else {
+    res.status(401).json({
+      message: 'User not found'
+    });
+  }
   res.status(200).json({
     login: req.body.email,
     password: req.body.password
