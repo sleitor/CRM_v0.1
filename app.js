fa61688890d552af348cc1c2757d181b18bbd055
DmitryT <8910140@mail.ru>
2019-02-10 21:36:32 +0300

    15. add morgan(logging request), add cors

--- app.js
+++ app.js
@@ -8,8 +8,11 @@ const categoryRoutes = require('./routes/category');
 const orderRoutes = require('./routes/order');
 const positionRoutes = require('./routes/position');
 
+app.use(require('morgan')('dev'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+app.use(require('cors')());
+
 
 app.use('/api/analytics', analyticsRoutes);
 app.use('/api/auth', authRoutes);
