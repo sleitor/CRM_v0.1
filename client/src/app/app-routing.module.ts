893d1e63399004f59e6a1e584eea1ca3ec87ae73
DmitryT <8910140@mail.ru>
2019-03-04 00:55:16 +0300

    35.2 Insert routerLink for link in layout, and create redirect to root path

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -10,6 +10,7 @@ const routes: Routes = [
     path: '',
     component: AuthLayoutComponent,
     children: [
+      {path: '', redirectTo: '/login', pathMatch: 'full'},
       {path: 'login', component: LoginPageComponent},
       {path: 'register', component: RegisterPageComponent}
     ],
