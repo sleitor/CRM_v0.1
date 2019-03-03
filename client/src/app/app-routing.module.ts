660958a970c14aca69990f2f7aaf022e0fe2f90f
DmitryT <8910140@mail.ru>
2019-03-04 00:46:21 +0300

    35.1 Create layout, create login, register components

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,9 +1,24 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {LoginPageComponent} from "./login-page/login-page.component";
+import {AuthLayoutComponent} from "./shared/layout/auth-layout/auth-layout.component";
+import {MainLayoutComponent} from "./shared/layout/main-layout/main-layout.component";
+import {RegisterPageComponent} from "./register-page/register-page.component";
 
 const routes: Routes = [
-  {path: 'login', component: LoginPageComponent}
+  {
+    path: '',
+    component: AuthLayoutComponent,
+    children: [
+      {path: 'login', component: LoginPageComponent},
+      {path: 'register', component: RegisterPageComponent}
+    ],
+  },
+  {
+    path: '',
+    component: MainLayoutComponent,
+    children: [],
+  },
 ];
 
 @NgModule({
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginPageComponent } from './login-page/login-page.component';
+import { AuthLayoutComponent } from './shared/layout/auth-layout/auth-layout.component';
+import { MainLayoutComponent } from './shared/layout/main-layout/main-layout.component';
+import { RegisterPageComponent } from './register-page/register-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginPageComponent
+    LoginPageComponent,
+    AuthLayoutComponent,
+    MainLayoutComponent,
+    RegisterPageComponent
   ],
   imports: [
     BrowserModule,
