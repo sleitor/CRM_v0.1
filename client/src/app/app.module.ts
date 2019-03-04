200a40829f0d8a5ce6a9aad891ec18239ec9a164
DmitryT <8910140@mail.ru>
2019-03-04 18:41:37 +0300

    36.1 Add Reactive forms to login form

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,12 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { LoginPageComponent } from './login-page/login-page.component';
-import { AuthLayoutComponent } from './shared/layout/auth-layout/auth-layout.component';
-import { MainLayoutComponent } from './shared/layout/main-layout/main-layout.component';
-import { RegisterPageComponent } from './register-page/register-page.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {LoginPageComponent} from './login-page/login-page.component';
+import {AuthLayoutComponent} from './shared/layout/auth-layout/auth-layout.component';
+import {MainLayoutComponent} from './shared/layout/main-layout/main-layout.component';
+import {RegisterPageComponent} from './register-page/register-page.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -18,9 +19,12 @@ import { RegisterPageComponent } from './register-page/register-page.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
