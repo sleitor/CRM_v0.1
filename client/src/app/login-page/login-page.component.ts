05f716886ede708ecbee4b376c9423b661049573
DmitryT <8910140@mail.ru>
2019-03-07 21:24:19 +0300

    37.3 prepare for message system in login page

--- client/src/app/login-page/login-page.component.ts
+++ client/src/app/login-page/login-page.component.ts
@@ -2,6 +2,7 @@ import {Component, OnDestroy, OnInit} from '@angular/core';
 import {FormControl, FormGroup, Validators} from "@angular/forms";
 import {AuthService} from "../shared/services/auth.service";
 import {Subscription} from "rxjs";
+import {ActivatedRoute, Params, Router} from "@angular/router";
 
 @Component({
   selector: 'app-login-page',
@@ -13,13 +14,24 @@ export class LoginPageComponent implements OnInit, OnDestroy {
   form: FormGroup;
   aSub: Subscription;
 
-  constructor(private auth: AuthService) {
+  constructor(
+    private auth: AuthService,
+    private router: Router,
+    private route: ActivatedRoute) {
   }
 
   ngOnInit() {
     this.form = new FormGroup({
       email: new FormControl(null, [Validators.required, Validators.email]),
       password: new FormControl(null, [Validators.required, Validators.minLength(6)]),
+    });
+
+    this.route.queryParams.subscribe((params: Params) => {
+      if (params['registered']) {
+        // then you can login to this system using your credentials
+      } else if (params['accessDenied']) {
+        // please login before
+      }
     })
   }
 
@@ -27,7 +39,7 @@ export class LoginPageComponent implements OnInit, OnDestroy {
     this.form.disable();
     console.log('Submit', this.form.value);
     this.aSub = this.auth.login(this.form.value).subscribe(
-      () => console.log('Login success'),
+      () => this.router.navigate(['/overview']),
       error => {
         console.warn('Error', error);
         this.form.enable();
