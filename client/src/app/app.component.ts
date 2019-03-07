c6b36c080fe0014d0919ca3ce4eae45a99b88a18
DmitryT <8910140@mail.ru>
2019-03-07 22:43:08 +0300

    40.2 load token from localstorage

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,8 +1,18 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AuthService} from "./shared/services/auth.service";
 
 @Component({
   selector: 'app-root',
   template: '<router-outlet></router-outlet>',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(
+    private auth: AuthService,
+  ) {
+  }
+
+  ngOnInit(): void {
+    const token = localStorage.getItem('token');
+    if (token) this.auth.setToken(token)
+  }
 }
