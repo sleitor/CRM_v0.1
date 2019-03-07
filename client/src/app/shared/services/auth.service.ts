5fbb58e44b84048baa2d3d2951a3d86fa80a709e
DmitryT <8910140@mail.ru>
2019-03-07 21:33:45 +0300

    37.4 write logic for work with token on frontend

--- client/src/app/shared/services/auth.service.ts
+++ client/src/app/shared/services/auth.service.ts
@@ -2,11 +2,14 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {User} from "../interfaces";
 import {Observable} from "rxjs";
+import {tap} from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+
+  private token: string = null;
 
   constructor(private http: HttpClient) {
   }
@@ -17,5 +20,28 @@ export class AuthService {
 
   login(user: User): Observable<{ token: string }> {
     return this.http.post<{ token: string }>('/api/auth/login', user)
+      .pipe(
+        tap(({token}) => {
+          localStorage.setItem('token', token);
+          this.setToken(token);
+        })
+      )
+  }
+
+  setToken(token): void {
+    this.token = token;
+  }
+
+  getToken(): string {
+    return this.token
+  }
+
+  isAuthenticated(): boolean {
+    return !!this.token;
+  }
+
+  logout(): void {
+    this.token = null;
+    localStorage.clear();
   }
 }
