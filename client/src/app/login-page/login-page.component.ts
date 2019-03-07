83709390d07dc93fb1d2293572a992f01b74115f
DmitryT <8910140@mail.ru>
2019-03-07 20:00:55 +0300

    36.2 Display error messages for login form

--- client/src/app/login-page/login-page.component.ts
+++ client/src/app/login-page/login-page.component.ts
@@ -16,7 +16,7 @@ export class LoginPageComponent implements OnInit {
   ngOnInit() {
     this.form = new FormGroup({
       email: new FormControl(null, [Validators.required, Validators.email]),
-      password: new FormControl(null, Validators.min(6))
+      password: new FormControl(null, [Validators.required, Validators.minLength(6)]),
     })
   }
 
