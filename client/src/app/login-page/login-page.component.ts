6590fe6378f79e9c92b64fbc41aaba7149d04abf
DmitryT <8910140@mail.ru>
2019-03-07 21:08:10 +0300

    37.2 minimize some memory leak

--- client/src/app/login-page/login-page.component.ts
+++ client/src/app/login-page/login-page.component.ts
@@ -1,15 +1,17 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {FormControl, FormGroup, Validators} from "@angular/forms";
 import {AuthService} from "../shared/services/auth.service";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.scss']
 })
-export class LoginPageComponent implements OnInit {
+export class LoginPageComponent implements OnInit, OnDestroy {
 
   form: FormGroup;
+  aSub: Subscription;
 
   constructor(private auth: AuthService) {
   }
@@ -24,12 +26,16 @@ export class LoginPageComponent implements OnInit {
   onSubmit() {
     this.form.disable();
     console.log('Submit', this.form.value);
-    this.auth.login(this.form.value).subscribe(
+    this.aSub = this.auth.login(this.form.value).subscribe(
       () => console.log('Login success'),
       error => {
         console.warn('Error', error);
         this.form.enable();
       },
     );
+  }
+
+  ngOnDestroy(): void {
+    if (this.aSub) this.aSub.unsubscribe();
   }
 }
