e54875ef91dac9f9fbb1e199bb1075a0011b6975
DmitryT <8910140@mail.ru>
2019-03-17 00:52:18 +0300

    60.3 Display order in popup window

--- client/src/app/history-page/history-table/history-table.component.ts
+++ client/src/app/history-page/history-table/history-table.component.ts
@@ -1,18 +1,38 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, OnDestroy, ViewChild } from '@angular/core';
 import { Order } from '../../shared/interfaces';
+import { MaterialInstance, MaterialService } from '../../shared/services/material.service';
 
 @Component({
   selector: 'app-history-table',
   templateUrl: './history-table.component.html',
-  styleUrls: ['./history-table.component.scss']
+  styleUrls: ['./history-table.component.scss'],
 })
-export class HistoryTableComponent implements OnInit {
+export class HistoryTableComponent implements OnDestroy, AfterViewInit {
 
   @Input() orders: Order[];
+  @ViewChild('modal') modalRef: ElementRef;
+  modal: MaterialInstance;
 
-  constructor() { }
+  selectedOrder: Order;
 
-  ngOnInit() {
+  ngAfterViewInit(): void {
+    this.modal = MaterialService.initModal(this.modalRef);
   }
 
+  ngOnDestroy(): void {
+    this.modal.destroy();
+  }
+
+  computeOrder(order: Order) {
+    return order.list.reduce((total, item) => total + item.cost * item.quantity, 0);
+  }
+
+  openModal(order: Order) {
+    this.selectedOrder = order;
+    this.modal.open();
+  }
+
+  closeModal() {
+    this.modal.close();
+  }
 }
