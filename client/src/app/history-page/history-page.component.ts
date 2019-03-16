c4206c83f9089a2387a38a83bf404c355213e543
DmitryT <8910140@mail.ru>
2019-03-17 00:18:39 +0300

    60.2 load more orders for history page

--- client/src/app/history-page/history-page.component.ts
+++ client/src/app/history-page/history-page.component.ts
@@ -8,7 +8,7 @@ const COUNT = 2;
 @Component({
   selector: 'app-history-page',
   templateUrl: './history-page.component.html',
-  styleUrls: ['./history-page.component.scss']
+  styleUrls: ['./history-page.component.scss'],
 })
 export class HistoryPageComponent implements OnInit, OnDestroy, AfterViewInit {
 
@@ -19,7 +19,11 @@ export class HistoryPageComponent implements OnInit, OnDestroy, AfterViewInit {
   isFilterShow = false;
   orders = [];
   limit = COUNT;
-  offset = 0;
+  skip = 0;
+
+  reloading = true;
+  loading = false;
+  noMoreLoad = false;
 
   constructor(
     private orderService: OrderService,
@@ -39,14 +43,25 @@ export class HistoryPageComponent implements OnInit, OnDestroy, AfterViewInit {
     this.oSup.unsubscribe();
   }
 
+  loadMore() {
+    this.loading = true;
+    this.skip += COUNT;
+    this.fetch();
+  }
+
   private fetch() {
     const params = {
-      offset: this.offset,
+      skip: this.skip,
       limit: this.limit,
     };
 
     this.oSup = this.orderService.fetch(params).subscribe(
-      orders => this.orders = orders,
+      orders => {
+        this.orders = this.orders.concat(orders);
+        this.loading = false;
+        this.reloading = false;
+        this.noMoreLoad = orders.length < COUNT;
+      },
     );
   }
 }
