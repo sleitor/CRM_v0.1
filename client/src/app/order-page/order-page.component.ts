f5cfa6339d4f6bb58f008ed287fea3f43e828ba2
DmitryT <8910140@mail.ru>
2019-03-16 04:18:15 +0300

    55.1 Breadcrumbs for order's page

--- client/src/app/order-page/order-page.component.ts
+++ client/src/app/order-page/order-page.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-order-page',
   templateUrl: './order-page.component.html',
   styleUrls: ['./order-page.component.scss']
 })
-export class OrderPageComponent implements OnInit {
+export class OrderPageComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  isRoot: boolean = this.router.url === '/order';
+  destroy$ = new Subject;
+
+  constructor(
+    private router: Router,
+  ) {
+  }
 
   ngOnInit() {
+    this.router.events.pipe(
+      takeUntil(this.destroy$),
+    )
+      .subscribe(event => {
+        if (event instanceof NavigationEnd) {
+          console.log(this.router.url, this.router.url === '/order');
+          this.isRoot = this.router.url === '/order';
+        }
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 
 }
